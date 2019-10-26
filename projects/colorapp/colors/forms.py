54adc4518acc4f69d4e3f44e6d7e3521f8ac6a0b
John Absher <jabsher@squarespace.com>
2019-10-26 11:59:13 -0400

    Adding in a ton of cosmetic upgrades and text

--- projects/colorapp/colors/forms.py
+++ projects/colorapp/colors/forms.py
@@ -3,6 +3,6 @@ from colors.validation import colormaps_valid_aligned
 
 
 class GetHexForm(forms.Form):
-    colormap = forms.ChoiceField(choices=[(x, x) for x in colormaps_valid_aligned], help_text="Select a Matlab color gradient.")
+    colormap = forms.ChoiceField(choices=[(x, x) for x in colormaps_valid_aligned], help_text="Select a Matlab color gradient.", initial='viridis')
     flip = forms.BooleanField(help_text='Check to flip the gradient', required=False)
-    n = forms.IntegerField(help_text="Input number of desired buckets.", min_value=0, max_value=1000)
+    n = forms.IntegerField(help_text="Input number of desired buckets.", min_value=0, max_value=1000, initial='5')
